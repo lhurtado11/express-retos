1da4c08e6c18749db9180867462987f769134ea3
Luisa <lhurtado372@gmail.com>
2021-03-23 19:34:03 -0500

    reto: saludame 1

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send(`<h1>Hola ${req.query.name}!</h1>`)
+    const name = req.query.nombre || 'desconocido'
+    res.send(`<h1>Hola ${name}!</h1>`)
 })
 app.listen(3000, () => console.log('Listening on port 3000!'))
