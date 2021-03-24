23531e42eaba614e134fe603403e03fa9ece3752
Luisa <lhurtado372@gmail.com>
2021-03-23 19:11:52 -0500

    reto:saludame 1

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-const express = require('express');
+//reto:codigo secreto
+/*const express = require('express');
 const app = express();
 
 app.get('/',(req, res) => {
     res.send('<h1>HcUy6Re2LLBRtj</h1>')
 })
 
+app.listen(3000, () => console.log('Listening on port 3000!'))
+*/
+//reto: saludame 1
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Hola ${req.query.name}!</h1>`)
+})
 app.listen(3000, () => console.log('Listening on port 3000!'))
