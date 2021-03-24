267006e18d4c92fb3782c836fca237dfc39733c0
Luisa <lhurtado372@gmail.com>
2021-03-23 19:53:02 -0500

    reto: saludame 2

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/',(req, res) => {
 })
 
 app.listen(3000, () => console.log('Listening on port 3000!'))
-*/
+
 //reto: saludame 1
 const express = require('express');
 const app = express();
@@ -15,5 +15,16 @@ const app = express();
 app.get('/', (req, res) => {
     const name = req.query.nombre || 'desconocido'
     res.send(`<h1>Hola ${name}!</h1>`)
+})
+app.listen(3000, () => console.log('Listening on port 3000!'))
+*/
+//reto: saludame 2
+const express = require('express');
+const app = express();
+
+app.get('/makers/:name', (req, res) => {
+    const name = req.params.name
+
+    res.send(`<h1>Hola ${name.replace(/^\w/, (c) => c.toUpperCase())}!</h1>`)
 })
 app.listen(3000, () => console.log('Listening on port 3000!'))
