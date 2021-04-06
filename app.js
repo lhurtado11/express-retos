38ba91a32cfc88859cd322ca591404c06b7e07ae
Luisa <lhurtado372@gmail.com>
2021-04-06 16:59:37 -0500

    reto saludame 3 listo

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-    res.send(`<h1> Hola ${req.body.name}!</h1>`);
+    res.send(`<h1> Hola ${req.body.name} !</h1>`);
 })
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
